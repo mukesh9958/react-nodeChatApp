d99237f571cee525cddd60c6be01d5e471d34afc
Mukesh Kumar <49620364+mukesh9958@users.noreply.github.com>
2023-01-25 20:29:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const messageRoutes = require("./routes/messages");
+const app = express();
+const socket = require("socket.io");
+
+const DB = "mongodb+srv://Abhishek:Minato%40123@cluster0.5lmre.mongodb.net/chat-snappy?retryWrites=true&w=majority";
+
+mongoose.connect(DB).then(() => {
+  console.log("connection successful");
+}).catch((err) => console.log(err));
+
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Server started on ${process.env.PORT}`)
+);
+const io = socket(server, {
+  cors: {
+    origin: "https://chat--app--server.herokuapp.com",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
+});
